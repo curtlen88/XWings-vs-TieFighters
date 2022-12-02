5f84fb77d89458c7d4010adc9ee962edcfee86f0
curtlen88 <curtlen88@gmail.com>
2022-12-01 17:38:31 -0700

    canvas container size fixed gameObjects rendered to screen

--- js/script.js
+++ js/script.js
@@ -3,8 +3,23 @@ const startButton = document.querySelector('#startButton')
 const restartButton = document.querySelector('#restartButton')
 console.log(startButton, restartButton)
 
+
+//reset canvas resolution to window size
+canvas.setAttribute('height', getComputedStyle(canvas)['height'])
+canvas.setAttribute('width', getComputedStyle(canvas)['width'])
+
+
+// get rendering context from the canvas
+const ctx = canvas.getContext('2d')
+console.log(ctx)
+
+// testing canvas rendering 
+// ctx.fillStyle ='red'
+// ctx.fillRect(0,0,150,300)
+
+
 // Event Liserners
-startButton.addEventListener('click',(startGame))
+startButton.addEventListener('click', startGame)
 restartButton.addEventListener('click', restartGame)
 
 // Functions
@@ -14,3 +29,30 @@ function startGame (){
 function restartGame (){
     console.log(`restart clicked`)
 }
+
+// Classes for on screen objects
+class gameObject {
+    constructor(x, y, width, height, color) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.color = color
+
+        this.alive = true
+    }
+
+    render () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x ,this.y, this.width, this.height)
+    }
+}
+// game variables 
+// const gameLoopInterval = setInterval(gameLoop, 60)
+const xWing = new gameObject(5, 145, 25, 25, 'hotpink')
+const tieFighter = new gameObject(50, 50, 50, 75, 'green')
+// const pressedKeys = {}
+
+// Render gameObjects
+xWing.render()
+tieFighter.render()
