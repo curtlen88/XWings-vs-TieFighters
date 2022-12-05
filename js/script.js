1a19dd42f8ad99ab7ce9fe401ca76c3a2cf58438
curtlen88 <curtlen88@gmail.com>
2022-12-05 12:06:35 -0700

    meteor-asteroids-comet objects added

--- js/script.js
+++ js/script.js
@@ -7,13 +7,19 @@ const timer = document.querySelector('#timer')
 const WIN_TIME = 12
 const XWING_SPEED = 15
 const randomSpeed = Math.floor(Math.random() * 20)+1
-
 const ENEMY_SPEED = randomSpeed
 const xWingImage = new Image()
     xWingImage.src = "../img/chickenXwing.png"
 const tieFighterImage = new Image()
     tieFighterImage.src = "../img/bowtiefighter.png"
-// console.log(startButton, pauseButton)
+const asteroid1Image = new Image()
+    asteroid1Image.src = "../img/asteroid1.png"
+const asteroid2Image = new Image()
+    asteroid2Image.src = "../img/asteroid2.png"
+const cometImage = new Image()
+    cometImage.src = "../img/comet.png"
+const meteorImage = new Image()
+    meteorImage.src = "../img/meteor.png"
 
 //reset canvas pixels window size
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
@@ -21,7 +27,6 @@ canvas.setAttribute('width', getComputedStyle(canvas)['width'])
 
 // get rendering context from the canvas
 const ctx = canvas.getContext('2d')
-// console.log(ctx)
 
 // Event Listeners
 startButton.addEventListener('click', startGame)
@@ -31,9 +36,8 @@ restartButton.addEventListener('click', restartGame)
 // sets an open variable in the global scope
 let secondsInterval
 
-// Functions
+// Functions for buttons
 function startGame (){
-    // console.log(`start clicked`)
     gameLoopInterval = setInterval(gameLoop, 60)
     secondsPassed = 0
     // interval for the runSeconds Function
@@ -44,7 +48,6 @@ function startGame (){
 function restartGame (){
     clearInterval(gameLoopInterval)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // console.log(`restart clicked`)
     xWing.alive === true
     xWing.x = 425
     xWing.y = 750
@@ -61,13 +64,10 @@ function restartGame (){
     gameLoopInterval = setInterval(gameLoop, 60)
     // // interval for the runSeconds Function
     secondsInterval = setInterval(runSecondsPassed,1000)
-    
 }
 
 function pauseGame (){
-    // console.log(`pause clicked`)
     clearInterval(gameLoopInterval)
-    // startButton.innerText = 'Resume'
 }
 
 // Classes for on screen objects
@@ -81,10 +81,7 @@ class gameObject {
 
         this.alive = true
     }
-
     render () {
-        // ctx.fillStyle = this.color
-        // ctx.fillRect(this.x ,this.y, this.width, this.height)
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
     }
 }
@@ -92,17 +89,19 @@ class gameObject {
 // const screenRight = parseFloat(getComputedStyle(canvas).height,10)
 // const screenBottom = parseFloat(getComputedStyle(canvas).width,10)
 let gameLoopInterval = {}
-const xWing = new gameObject(xWingImage, 425, 750, 150, 150)
-const tieFighter1 = new gameObject(tieFighterImage, 1000, 450, 150, 150)
-const tieFighter2 = new gameObject(tieFighterImage, 1000, 350, 150, 150)
-const tieFighter3 = new gameObject(tieFighterImage, 1000, 250, 150, 150)
-const tieFighter4 = new gameObject(tieFighterImage, 1000, 150, 150, 150)
-const tieFighter5 = new gameObject(tieFighterImage, 1000, 100, 150, 150)
-const tieFighter6 = new gameObject(tieFighterImage, 1000, 75, 150, 150)
-const tieFighter7 = new gameObject(tieFighterImage, 1000, 25, 150, 150)
-const tieFighter8 = new gameObject(tieFighterImage, 1000, 15, 150, 150)
-const tieFighter9 = new gameObject(tieFighterImage, 1000, 5, 150, 150)
-const tieFighterArray = [tieFighter1, tieFighter2, tieFighter3, tieFighter4, tieFighter5, tieFighter6, tieFighter7, tieFighter8, tieFighter9]
+// new game objects
+const xWing = new gameObject(xWingImage, 425, 800, 150, 150)
+const tieFighter1 = new gameObject(tieFighterImage, 1000, 750, 100, 100)
+const tieFighter2 = new gameObject(tieFighterImage, 1000, 650, 100, 100)
+const tieFighter3 = new gameObject(tieFighterImage, 1000, 550, 100, 100)
+const tieFighter4 = new gameObject(tieFighterImage, 1000, 450, 100, 100)
+const tieFighter5 = new gameObject(tieFighterImage, 1000, 350, 100, 100)
+const asteroid1 = new gameObject(asteroid1Image, 1000, 250, 100, 100)
+const asteroid2 = new gameObject(asteroid2Image, 1000, 450, 100, 100)
+const comet = new gameObject(cometImage, 1000, 50, 100, 100)
+const meteor = new gameObject(meteorImage, 1000, 5, 100, 100)
+// array of enemy objects
+const enemyArray = [tieFighter1, tieFighter2, tieFighter3, tieFighter4, tieFighter5, asteroid1, asteroid2, comet, meteor]
 
 const pressedKeys = {}
 // Render gameObjects
@@ -112,25 +111,20 @@ xWing.render()
 function xWingMovement(speed) {
     // logic for moving the player around
     if (pressedKeys.ArrowUp && xWing.y > 0) {
-        // console.log(pressedKeys)
         xWing.y -= speed
     }
     if (pressedKeys.ArrowDown && xWing.y < 900 - xWing.height) {
-        // console.log(pressedKeys)
         xWing.y += speed
     }
     if (pressedKeys.ArrowRight && xWing.x < 1000 - xWing.width) {
-        // console.log(pressedKeys)
         xWing.x += speed
     }
     if (pressedKeys.ArrowLeft && xWing.x > 0) {
-        // console.log(pressedKeys)
         xWing.x -= speed
     }
 }
 
 //Event Listeners for keydown event
-// document.addEventListener('keydown', xWingMovement)
 document.addEventListener('keydown', e => pressedKeys[e.key] = true)
 document.addEventListener('keyup', e => pressedKeys[e.key] = false)
 
@@ -148,19 +142,16 @@ let secondsPassed = 0
 // adds to the seconds
 function runSecondsPassed() {
     secondsPassed++
-    // console.log(secondsPassed)
 }
 
 // Define the game loop -> what happens when the game is running
 function gameLoop(){
-    // console.log(gameLoopInterval)
     // clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     timer.innerText = `Timer: ${secondsPassed} Seconds`
     //if the xwing hits an object end the game 
-    tieFighterArray.forEach(tieFighter => {
+    enemyArray.forEach(tieFighter => {
         if(detectCollision(xWing,tieFighter)) {
-                    // console.log('object hit')
         xWing.alive = false
         // display you died message 
         statusMessage.innerText = ('You Died')
@@ -182,10 +173,10 @@ function gameLoop(){
     tieFighter3.render()
     tieFighter4.render()
     tieFighter5.render()
-    tieFighter6.render()
-    tieFighter7.render()
-    tieFighter8.render()
-    tieFighter9.render()
+    asteroid1.render()
+    asteroid2.render()
+    comet.render()
+    meteor.render()
 }
 
 //from canvas crawler (link)
@@ -195,7 +186,6 @@ function detectCollision (xWing, enemy) {
         const right = xWing.x <= enemy.x + enemy.width
         const top = xWing.y + xWing.height >= enemy.y
         const bottom = xWing.y <= enemy.y + enemy.height
-        // console.log(left, right, top, bottom)
         return left && right && top && bottom
 }
 
